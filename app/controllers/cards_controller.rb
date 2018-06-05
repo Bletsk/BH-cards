fff21b0eb7b4023bd6d575baa1f43a91e6970e5d
Bletsk <blackrid2009@gmail.com>
2018-06-06 01:57:25 +0300

    partial reload

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,32 +1,42 @@
 class CardsController < ApplicationController
 	def index
-		is_hero_dropped = Random.rand(8) == 0
-
-		set_id = 1
-
-		card_pull = Card.where("card_set_id = ? AND dropped_from_booster = true", set_id)
-
-		@cards = []
-
-		if is_hero_dropped
-			@hero = card_pull.where("card_type = 'hero' AND rarity = 'common'").shuffle.first
-			@cards << @hero
-		end
-
-		@rare = Random.rand(8) == 0 ? card_pull.where(rarity: "ultra").where.not(card_type: "hero").shuffle.first : card_pull.where(rarity: "rare").where.not(card_type: "hero").shuffle.first
-		@cards << @rare
-
-		card_pull.where(rarity: "uncommon").where.not(card_type: "hero").shuffle[0 .. 2].each do |card|
-			@cards << card
-		end
-
-		common = is_hero_dropped ? card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 9] : card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 10]
-		common.each do |card|
-			@cards << card
-		end
+		@cards = generate_pack(1)
 
 		# render json: @cards
 
 		# render json: card_pull
+	end
+
+	def show_cards
+		# @i = Card.where("card_set_id = ?", 1).all.length
+
+		@cards = generate_pack(1)
+	end
+
+	def generate_pack(set_id)
+		is_hero_dropped = Random.rand(8) == 0
+
+		card_pull = Card.where("card_set_id = ? AND dropped_from_booster = true", set_id)
+
+		cards = []
+
+		if is_hero_dropped
+			hero = card_pull.where("card_type = 'hero' AND rarity = 'common'").shuffle.first
+			cards << hero
+		end
+
+		rare = Random.rand(8) == 0 ? card_pull.where(rarity: "ultra").where.not(card_type: "hero").shuffle.first : card_pull.where(rarity: "rare").where.not(card_type: "hero").shuffle.first
+		cards << rare
+
+		card_pull.where(rarity: "uncommon").where.not(card_type: "hero").shuffle[0 .. 2].each do |card|
+			cards << card
+		end
+
+		common = is_hero_dropped ? card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 9] : card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 10]
+		common.each do |card|
+			cards << card
+		end
+
+		return cards
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root :to => "cards#index"
 
 	resources :cards, only: [:index]
+	get '/cards/generate_cards', to: 'cards#show_cards', as: "generate"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
