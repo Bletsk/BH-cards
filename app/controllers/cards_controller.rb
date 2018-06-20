44cf7e02f2b4f7391f2af94164decc3b3592a238
Bletsk <blackrid2011@gmail.com>
2018-06-20 12:55:09 +0300

    quick fix

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -22,18 +22,18 @@ class CardsController < ApplicationController
 		card_pull = Card.where("card_set_id = ? AND dropped_from_booster = true", set_id)
 
 		if is_hero_dropped
-			hero = card_pull.where("card_type = 'hero'").shuffle.first
+			hero = card_pull.where("card_type.name = 'Герой'").shuffle.first
 			cards << hero
 		end
 
-		rare = Random.rand(8) == 0 ? card_pull.where(rarity: "ultra").where.not(card_type: "hero").shuffle.first : card_pull.where(rarity: "rare").where.not(card_type: "hero").shuffle.first
+		rare = Random.rand(8) == 0 ? card_pull.where(rarity: "ultra").shuffle.first : card_pull.where(rarity: "rare").shuffle.first
 		cards << rare
 
-		card_pull.where(rarity: "uncommon").where.not(card_type: "hero").shuffle[0 .. 2].each do |card|
+		card_pull.where(rarity: "uncommon").shuffle[0 .. 2].each do |card|
 			cards << card
 		end
 
-		common = is_hero_dropped ? card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 9] : card_pull.where(rarity: "common").where.not(card_type: "hero").shuffle[0 .. 10]
+		common = is_hero_dropped ? card_pull.where(rarity: "common").shuffle[0 .. 9] : card_pull.where(rarity: "common").shuffle[0 .. 10]
 		common.each do |card|
 			cards << card
 		end
