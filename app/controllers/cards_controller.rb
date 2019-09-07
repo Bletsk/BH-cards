10c7aaa660d668ee3de46d3ea7b6222de4a6997c
Bletsk <s.tomashevsky@oblakogroup.ru>
2019-09-07 16:35:55 +0300

    Rubocop fixes applied

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -3,21 +3,21 @@
 class CardsController < ApplicationController
   def index
     @cards = init_generator
-    @heroes = getHeroes
+    @heroes = get_heroes
   end
 
   def show_cards
     # render json: params[:set]
     set = params[:set]
     @cards = init_generator([set[0].to_i, set[1].to_i, set[2].to_i, set[3].to_i])
-    @heroes = getHeroes([set[0].to_i, set[1].to_i, set[2].to_i, set[3].to_i])
+    @heroes = get_heroes([set[0].to_i, set[1].to_i, set[2].to_i, set[3].to_i])
   end
 
   def generate_pack(set_id = 6)
     cards = []
-    return cards if set_id == 0
+    return cards if set_id.zero?
 
-    is_hero_dropped = Random.rand(8) == 0
+    is_hero_dropped = Random.rand(8).zero?
 
     card_pull = Card.where('card_set_id = ? AND dropped_from_booster = true', set_id)
 
@@ -26,14 +26,22 @@ class CardsController < ApplicationController
       cards << hero
     end
 
-    rare = Random.rand(8) == 0 ? card_pull.where(rarity: 'ultra').where.not(card_type: 'hero').sample : card_pull.where(rarity: 'rare').where.not(card_type: 'hero').sample
+    rare = if Random.rand(8).zero?
+             card_pull.where(rarity: 'ultra').where.not(card_type: 'hero').sample
+           else
+             card_pull.where(rarity: 'rare').where.not(card_type: 'hero').sample
+           end
     cards << rare
 
     card_pull.where(rarity: 'uncommon').where.not(card_type: 'hero').sample(3).each do |card|
       cards << card
     end
 
-    common = is_hero_dropped ? card_pull.where(rarity: 'common').where.not(card_type: 'hero').sample(10) : card_pull.where(rarity: 'common').where.not(card_type: 'hero').sample(11)
+    common = if is_hero_dropped
+               card_pull.where(rarity: 'common').where.not(card_type: 'hero').sample(10)
+             else
+               card_pull.where(rarity: 'common').where.not(card_type: 'hero').sample(11)
+             end
     common.each do |card|
       cards << card
     end
@@ -41,28 +49,28 @@ class CardsController < ApplicationController
     cards
   end
 
-  def init_generator(set_ids = [6, 6, 6, 6], amountOfPlayers = 8)
+  def init_generator(set_ids = [6, 6, 6, 6], amount_of_players = 8)
     cards = []
     set_ids.each do |set_id|
-      _booster = []
-      (1..amountOfPlayers).each do |_i|
-        _booster << generate_pack(set_id)
+      booster = []
+      amount_of_players.times do
+        booster << generate_pack(set_id)
       end
-      cards << _booster
+      cards << booster
     end
 
     cards
   end
 
-  def getHeroes(set_ids = [6, 6, 6, 6])
-    setHash = {}
+  def get_heroes(set_ids = [6, 6, 6, 6])
+    set_hash = {}
     heroes = []
     set_ids.each do |set_id|
-      setHash[set_id] = true
+      set_hash[set_id] = true
     end
     heroes_pull = []
 
-    setHash.each do |key, value|
+    set_hash.each do |key, value|
       heroes_pull << Card.where("card_set_id = ? AND card_type = 'hero'", key)
       p key
       p value
