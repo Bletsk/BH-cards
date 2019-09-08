303f192e758e15a2304650b8d258fce24f25776d
Bletsk <blackrid2011@gmail.com>
2019-09-08 12:03:05 +0300

    WIP cleaning up views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,17 +1,28 @@
 # frozen_string_literal: true
 
 class CardsController < ApplicationController
+  helper_method :sets
+
   def draft
-    sets = (1..4).map { |_| params[:set_id].to_i || 1 }
+    @current_set = params[:set_id].to_i
+    sets = (1..4).map { |_| @current_set || 1 }
     @cards = init_generator(sets)
     @heroes = Card.heroes(sets.uniq)
   end
 
   def show_cards
-    set = params[:set].map(&:to_i)
-    @cards = init_generator(set)
-    @heroes = Card.heroes(set.uniq)
+    sets = params[:set].map(&:to_i)
+    @cards = init_generator(sets)
+    @heroes = Card.heroes(sets.uniq)
   end
+
+  def sets
+    { 'Время героев' => 1, 'Сила и честь' => 2, 'Повелители стихий' => 3, 'Восхождение тени' => 4,
+      'Рожденные бурей' => 5, 'Грехи прошлого' => 6, 'Расцвет технологий' => 7, 
+      'Цена прогресса' => 8 }
+  end
+
+  private
 
   def init_generator(set_ids = [1, 1, 1, 1], amount_of_players = 8)
     set_ids.each_with_object([]) do |set_id, cards|
