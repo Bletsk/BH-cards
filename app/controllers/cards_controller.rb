90591dafb9436109810f24de768b887455d204f2
Bletsk <blackrid2011@gmail.com>
2019-09-08 11:19:00 +0300

    Clean up card model and controller

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,85 +4,18 @@ class CardsController < ApplicationController
   def draft
     sets = (1..4).map { |_| params[:set_id].to_i || 1 }
     @cards = init_generator(sets)
-    @heroes = get_heroes(sets)
+    @heroes = Card.heroes(sets.uniq)
   end
 
   def show_cards
-    set = params[:set]
-    @cards = init_generator([set[0].to_i, set[1].to_i, set[2].to_i, set[3].to_i])
-    @heroes = get_heroes([set[0].to_i, set[1].to_i, set[2].to_i, set[3].to_i])
+    set = params[:set].map(&:to_i)
+    @cards = init_generator(set)
+    @heroes = Card.heroes(set.uniq)
   end
 
-  def generate_pack(set_id = 6)
-    cards = []
-    return cards if set_id.zero?
-
-    is_hero_dropped = Random.rand(8).zero?
-
-    card_pull = Card.where(card_set: set_id, dropped_from_booster: true)
-
-    if is_hero_dropped
-      hero = card_pull.where(cost: nil).sample
-      cards << hero
+  def init_generator(set_ids = [1, 1, 1, 1], amount_of_players = 8)
+    set_ids.each_with_object([]) do |set_id, cards|
+      cards << (1..amount_of_players).map { |_| Card.generate_pack(set_id) }
     end
-
-    rare = if Random.rand(8).zero?
-             card_pull.where(rarity: :ultra).where.not(cost: nil).sample
-           else
-             card_pull.where(rarity: :rare).where.not(cost: nil).sample
-           end
-    cards << rare
-
-    card_pull.where(rarity: :uncommon).where.not(cost: nil).sample(3).each do |card|
-      cards << card
-    end
-
-    common = if is_hero_dropped
-               card_pull.where(rarity: :common).where.not(cost: nil).sample(10)
-             else
-               card_pull.where(rarity: :common).where.not(cost: nil).sample(11)
-             end
-    common.each do |card|
-      cards << card
-    end
-
-    cards
-  end
-
-  def init_generator(set_ids = [6, 6, 6, 6], amount_of_players = 8)
-    cards = []
-    set_ids.each do |set_id|
-      booster = []
-      amount_of_players.times do
-        booster << generate_pack(set_id)
-      end
-      cards << booster
-    end
-
-    cards
-  end
-
-  def get_heroes(set_ids = [6, 6, 6, 6])
-    set_hash = {}
-    heroes = []
-    set_ids.each do |set_id|
-      set_hash[set_id] = true
-    end
-    heroes_pull = []
-
-    set_hash.each do |key, value|
-      heroes_pull << Card.where(card_set: key, cost: nil)
-      p key
-      p value
-    end
-
-    heroes_pull.each do |set|
-      set.each do |hero|
-        p hero
-        heroes << hero
-      end
-    end
-
-    heroes
   end
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -16,4 +16,30 @@ class Card < ApplicationRecord
     mountain: 4,
     neutral: 5
   }
+
+  def self.generate_pack(set_id = 1)
+    cards = []
+
+    is_hero_dropped = Random.rand(8).zero?
+    is_ultra_dropped = Random.rand(8).zero?
+
+    card_pull = Card.where(card_set: set_id, dropped_from_booster: true)
+
+    cards << card_pull.where(cost: nil).order('RANDOM()').first if is_hero_dropped
+    cards << card_pull.where(rarity: is_ultra_dropped ? :ultra : :rare)
+                      .where.not(cost: nil)
+                      .order('RANDOM()')
+                      .first
+    cards += card_pull.where(rarity: :uncommon).order('RANDOM()').first(3)
+    cards += card_pull.where(rarity: :common)
+                      .where.not(cost: nil)
+                      .order('RANDOM()')
+                      .first(is_hero_dropped ? 10 : 11)
+
+    cards
+  end
+
+  def self.heroes(set_ids = 1)
+    Card.where(card_set: set_ids, cost: nil)
+  end
 end
