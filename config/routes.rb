e4817a5fe6291fd266402f245857e8fe753c732b
Sergey Tomashevsky <s.tomashevsky@oblakogroup.ru>
2019-12-18 23:24:15 +0300

    Removed dead endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   root to: 'cards#draft'
 
   get '/draft/:set_id', to: 'cards#draft', as: 'draft'
-  get '/cards/generate_cards', to: 'cards#show_cards', as: 'generate'
 end
